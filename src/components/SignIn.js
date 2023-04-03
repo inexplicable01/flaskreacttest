ea1d57a339dfa4a437d6105083a3a5fe8a3505ed
Waichak Luk <waichak.luk@gmail.com>
2023-04-02 22:14:34 -0700

    Removed Google and Facebook Sign in for now
    Removed Google and Facebook Sign in for now

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -1,6 +1,6 @@
 import React from "react";
-import GoogleLogin from "react-google-login";
-import FacebookLogin from "react-facebook-login";
+// import GoogleLogin from "react-google-login";
+// import FacebookLogin from "react-facebook-login";
 import "./SignIn.css";
 
 function SignIn(props) {
@@ -16,19 +16,19 @@ function SignIn(props) {
     <div className="SignIn">
       <h2>Sign In</h2>
       <div className="SignInCard">
-        <GoogleLogin
-          clientId="your-google-client-id"
-          buttonText="Sign in with Google"
-          onSuccess={responseGoogle}
-          onFailure={responseGoogle}
-          cookiePolicy={"single_host_origin"}
-        />
-        <FacebookLogin
-          appId="your-facebook-app-id"
-          fields="name,email,picture"
-          callback={responseFacebook}
-          icon="fa-facebook"
-        />
+        {/*<GoogleLogin*/}
+        {/*  clientId="your-google-client-id"*/}
+        {/*  buttonText="Sign in with Google"*/}
+        {/*  onSuccess={responseGoogle}*/}
+        {/*  onFailure={responseGoogle}*/}
+        {/*  cookiePolicy={"single_host_origin"}*/}
+        {/*/>*/}
+        {/*<FacebookLogin*/}
+        {/*  appId="your-facebook-app-id"*/}
+        {/*  fields="name,email,picture"*/}
+        {/*  callback={responseFacebook}*/}
+        {/*  icon="fa-facebook"*/}
+        {/*/>*/}
       </div>
     </div>
   );
