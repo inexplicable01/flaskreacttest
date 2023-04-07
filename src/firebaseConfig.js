f568ddeaea58479a8925642a67c828ced2be4edb
Waichak Luk <waichak.luk@gmail.com>
2023-04-06 23:21:46 -0700

    Updated code for netlify/run build

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,4 +1,4 @@
-import firebase, {initializeApp} from 'firebase/app';
+import {initializeApp} from 'firebase/app';
 import {getFirestore} from 'firebase/firestore';
 import {getAnalytics} from "firebase/analytics";
 import {getAuth} from 'firebase/auth';
